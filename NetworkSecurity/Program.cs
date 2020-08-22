dc108a1fa2f0067e46b0afa05b42481001188d3d
hanshubin <hanshubin@xd.com>
2020-08-22 13:20:49 +0800

    Initialized network security repository.

---
+++ NetworkSecurity/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkSecurity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string stringToEncrypt, sKey, stringToDecrypt;
+                Console.WriteLine("Please enter your string:");
+                stringToEncrypt = Console.ReadLine();
+                Console.WriteLine("Please enter your key:");
+                sKey = Console.ReadLine();
+                stringToDecrypt = Encrypt(stringToEncrypt, sKey);
+                Console.WriteLine("Now the stringToEncrypt is " + stringToEncrypt + " and the sKey is " + sKey);
+                Console.WriteLine("The current result is " + stringToDecrypt);
+                stringToEncrypt = Decrypt(stringToDecrypt, sKey);
+                Console.WriteLine("Now the decrypted result is " + stringToEncrypt);
+            }
+        }
+
+        static public string Encrypt(string stringToEncrypt, string sKey)
+        {
+            DESCryptoServiceProvider des = new DESCryptoServiceProvider();
+            byte[] inputByteArray = Encoding.GetEncoding("UTF-8").GetBytes(stringToEncrypt);
+            des.Key = ASCIIEncoding.UTF8.GetBytes(sKey);
+            des.IV = ASCIIEncoding.UTF8.GetBytes(sKey);
+            System.IO.MemoryStream ms = new MemoryStream();
+            CryptoStream cs = new CryptoStream(ms, des.CreateEncryptor(), CryptoStreamMode.Write);
+            cs.Write(inputByteArray, 0, inputByteArray.Length);
+            cs.FlushFinalBlock();
+            StringBuilder ret = new StringBuilder();
+            foreach (byte b in ms.ToArray())
+            {
+                ret.AppendFormat("{0:X2}", b);
+            }
+            ret.ToString();
+            return ret.ToString();
+        }
+
+        static public string Decrypt(string stringToDecrypt, string sKey)
+        {
+            DESCryptoServiceProvider des = new DESCryptoServiceProvider();
+            byte[] inputByteArray = new byte[stringToDecrypt.Length / 2];
+            for (int x = 0; x < stringToDecrypt.Length / 2; x++)
+            {
+                int i = (Convert.ToInt32(stringToDecrypt.Substring(x * 2, 2), 16));
+                inputByteArray[x] = (byte)i;
+            }
+            des.Key = ASCIIEncoding.UTF8.GetBytes(sKey);
+            des.IV = ASCIIEncoding.UTF8.GetBytes(sKey);
+            MemoryStream ms = new MemoryStream();
+            CryptoStream cs = new CryptoStream(ms, des.CreateDecryptor(), CryptoStreamMode.Write);
+            cs.Write(inputByteArray, 0, inputByteArray.Length);
+            cs.FlushFinalBlock();
+            StringBuilder ret = new StringBuilder();
+            return System.Text.Encoding.Default.GetString(ms.ToArray());
+        }
+    }
+}
